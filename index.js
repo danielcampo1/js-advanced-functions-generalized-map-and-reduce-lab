09b63adb2e306d5ac0e1682585e27908b46d9967
github username <github email address>
2021-03-04 14:25:51 -0600

    commit tree ee2ca8db05782f2637a3eb8d344b1a6df041f467

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(array, cb) {
+    return array.map(num => cb(num))
+}
+
+function reduce(array, cb, start=0) {
+    let r = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < array.length; i++) {
+      r = cb(array[i], r)
+    }
+  
+    return r;
+}
